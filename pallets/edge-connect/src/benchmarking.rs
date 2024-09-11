ba20a3a7fe737d233c781461aba26f2c7b50fd71
Onggo Wahyudi <carnegzs@gmail.com>
2024-09-11 23:56:38 +0800

    SCRUM-75 Benchmark Fix

--- pallets/edge-connect/src/benchmarking.rs
+++ pallets/edge-connect/src/benchmarking.rs
@@ -10,23 +10,38 @@ use frame_support::sp_runtime::traits::One;
 use frame_support::BoundedVec;
 use frame_system::RawOrigin;
 
+
+
+
+// Helper function to convert the domain string into a BoundedVec with a maximum length of 128 bytes.
+// Convert the domain string into a vector of bytes and then into a BoundedVec with a maximum length of 128 bytes.
+// The `try_from` mthod ensure that the length of the string doesn't exceed the limit.
+fn get_domain(domain_str: &str) -> BoundedVec<u8, ConstU32<128>> {
+    BoundedVec::try_from(domain_str.as_bytes().to_vec()).expect("Domain string exceeds maximum length")
+}
+
+
 #[benchmarks]
 mod benchmarks {
 	use super::*;
 
 	#[benchmark]
 	fn register_worker() {
+		// Retrieve a whitelisted account to use as the caller (sender of the transaction)
+		// This is a pre-authorized account used for benchmarking purposes.
 		let caller: T::AccountId = whitelisted_caller();
 
-		let domain_str = "https://some_api_domain.com";
-		let domain_vec = domain_str.as_bytes().to_vec();
-		let domain: BoundedVec<u8, ConstU32<128>> = BoundedVec::try_from(domain_vec).unwrap();
+		// Define a domain string that will be part of the WorkerAPI (represent the worker's domain).
+		let domain = get_domain("https://some_api_domain.com");
 
+		// Create a WorkerAPI struct that contains the domain information.
 		let api_info = WorkerAPI {
-			ip: None,
-			domain: Some(domain),
+			domain, // Store the domain in the API Info
 		};
 
+		// Create a Worker struct with the relevant details.
+		// The worker is assigned an ID of 0, owned by the caller, start at block 1, and  is initially inactive.
+		// The worker's API information includes the domain created above.
 		let worker = Worker {
 			id: 0,
 			owner: caller.clone(),
@@ -35,13 +50,18 @@ mod benchmarks {
 			api: api_info.clone(),
 		};
 
+		// Call the `register_worker` extrinsic using the #[extrinsic_call] macro, which will benchmark the extrinsic.
+		// The `RawOrigin::Signed(caller.clone())` indicates that this transaction is signed by the caller.
+		// The domain  from `api_info` is passed as part of the worker registration process.
 		#[extrinsic_call]
 		register_worker(
 			RawOrigin::Signed(caller.clone()),
-			api_info.ip,
 			api_info.domain,
 		);
 
+		// After the worker is registered, assert that the worker was correctly stored in the pallet's storage.
+		// `get_worker_cluster` (getter fn for workercluster) retrieves the worker by the tuple (caller, worker ID).
+		// If the worker exists in storage, the assertion passes, indicating that the registration succeeded.
 		assert_eq!(
 			edgeConnectPallet::<T>::get_worker_clusters((caller.clone(), 0)),
 			Some(worker)
@@ -50,36 +70,35 @@ mod benchmarks {
 
 	#[benchmark]
 	fn remove_worker() {
+		// Retrieve a whitelisted account to use as the caller (sender of the transaction)
+		// This is a pre-authorized account used for benchmarking purposes.
 		let caller: T::AccountId = whitelisted_caller();
 
+		// Define a domain string that will be part of the WorkerAPI (represent the worker's domain).
+		let domain = get_domain("https://some_api_domain.com");
+
 		// First, register a worker to ensure there is one to remove
+		// This step simulates the existence of a worker in the system before we benchmark the removal process.
 		let api_info = WorkerAPI {
-			ip: Some(Ip {
-				ipv4: Some(125),
-				ipv6: None,
-				port: 123,
-			}),
-			domain: None,
+			domain
 		};
 
-		let worker = Worker {
-			id: 0,
-			owner: caller.clone(),
-			start_block: <frame_system::Pallet<T>>::block_number(),
-			status: WorkerStatusType::Inactive,
-			api: api_info.clone(),
-		};
-
+		// Call the `register_worker` extrinsic to create and register a worker.
+		// This step is necessary before calling `remove_worker` because we need to remove an existing worker.
 		edgeConnectPallet::<T>::register_worker(
 			RawOrigin::Signed(caller.clone()).into(),
-			api_info.ip,
 			api_info.domain,
 		)
 		.unwrap();
 
+		// Call the `remove_worker` extrinsic using the #[extrinsic_call] macro, which will benchmark the extrinsic.
+		// the `RawOrigin::Signed(caller.clone())` indicates that this transaction is signed by the caller.
+		// Remove the worker with ID 0
 		#[extrinsic_call]
 		remove_worker(RawOrigin::Signed(caller.clone()), 0);
 
+		// After the `remove_worker` extrinsic is called, assert that the worker has been succesfully removed from storage.
+		// `get_worker_cluster` (getter fn for workercluster) should return `None` for the worker with ID 0, indicating the worker no longer exists.
 		assert_eq!(
 			edgeConnectPallet::<T>::get_worker_clusters((caller.clone(), 0)),
 			None
--- pallets/task-management/src/benchmarking.rs
+++ pallets/task-management/src/benchmarking.rs
@@ -6,55 +6,91 @@ use frame_support::BoundedVec;
 use frame_system::RawOrigin;
 use sp_core::H256;
 
+// Helper function to convert the domain string into a BoundedVec with a maximum length of 128 bytes.
+// Convert the domain string into a vector of bytes and then into a BoundedVec with a maximum length of 128 bytes.
+// The `try_from` mthod ensure that the length of the string doesn't exceed the limit.
+fn get_domain(domain_str: &str) -> BoundedVec<u8, ConstU32<128>> {
+    BoundedVec::try_from(domain_str.as_bytes().to_vec()).expect("Domain string exceeds maximum length")
+}
+
+// Helper function to convert task data into a BoundedVec with a maximum length of 128 bytes.
+// This ensures the task data string does not exceed the limit of 128 bytes.
+fn get_task_data(task_data_str: &str) -> BoundedVec<u8, ConstU32<128>> {
+    BoundedVec::try_from(task_data_str.as_bytes().to_vec()).expect("Task Data string exceeds maximum length")
+}
+
+
+
 benchmarks! {
+		// Benchmark for scheduling a task
 		task_scheduler {
 				let s in 0 .. 100;
+
+				// Retrieve a whitelisted account to use as the caller (sender of the transaction)
+				// This is a pre-authorized account used for benchmarking purposes.
 				let caller: T::AccountId = whitelisted_caller();
 
-				// Register a worker for the caller
-				let api_info = pallet_edge_connect::types::WorkerAPI {
-						ip: None,
-						domain: Some(BoundedVec::try_from(b"https://api-worker.testing".to_vec()).unwrap())
-				};
-				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(caller.clone()).into(), api_info.ip, api_info.domain)?;
+				// Get the domain for the worker.
+				let domain=get_domain("https://api-worker.testing");
 
-				let task_data = BoundedVec::try_from(b"some-docker-imgv.0".to_vec()).unwrap();
+				// Register a worker for the caller with the specified domain.
+				let api_info = pallet_edge_connect::types::WorkerAPI {
+						domain,
+				};
+
+				// Register a worker for the caller with the specified domain.
+				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(caller.clone()).into(), api_info.domain)?;
+
+				// Create task data.
+				let task_data=get_task_data("some-docker-imgv.0");
+
+        // Benchmark the task scheduling
 		}: _(RawOrigin::Signed(caller.clone()), task_data.clone())
 		verify {
+			    // Verify that the task has been correctly scheduled.
 				let task_id = TaskManagementModule::<T>::next_task_id() - 1;
 				let task_info = TaskManagementModule::<T>::get_tasks(task_id).unwrap();
 				assert_eq!(task_info.metadata, task_data);
 				assert_eq!(task_info.task_owner, caller);
 		}
 
+		// Benchmark for submitting a completed task
 		submit_completed_task {
 				let s in 0 .. 100;
+				
+				// Retrieve a whitelisted account to use as the caller (sender of the transaction)
+				// This is a pre-authorized account used for benchmarking purposes.
+				// Retrieve the caller and register the executor.
 				let caller: T::AccountId = whitelisted_caller();
-
-				// Register a worker for the executor
 				let executor: T::AccountId = account("executor", 0, 0);
+				
+				// Register the executor worker with a domain.
+				let domain=get_domain("https://api-worker.testing");
 				let api_info = pallet_edge_connect::types::WorkerAPI {
-						ip: None,
-						domain: Some(BoundedVec::try_from(b"https://api-worker.testing".to_vec()).unwrap())
+						domain,
 				};
-				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(executor.clone()).into(), api_info.ip, api_info.domain)?;
+				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(executor.clone()).into(), api_info.domain)?;
 
-				// Create a task data
-				let task_data = BoundedVec::try_from(b"some-docker-imgv.0".to_vec()).unwrap();
+				// Create task data and schedule the task.
+				let task_data = get_task_data("some-docker-imgv.0");
 				TaskManagementModule::<T>::task_scheduler(RawOrigin::Signed(caller.clone()).into(), task_data.clone())?;
 
-				// Register a worker for the verifier
+				// Register the verifier with a different domain.
 				let verifier: T::AccountId = account("verifier", 0, 0);
+				let domain2=get_domain("https://api-worker2.testing");
 				let api_info_verifier = pallet_edge_connect::types::WorkerAPI {
-						ip: None,
-						domain: Some(BoundedVec::try_from(b"https://api-worker2.testing".to_vec()).unwrap())
+						domain:domain2,
 				};
-				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(verifier.clone()).into(), api_info_verifier.ip, api_info_verifier.domain)?;
+				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(verifier.clone()).into(), api_info_verifier.domain)?;
 
+				// Complete the task by the executor.
 				let task_id = TaskManagementModule::<T>::next_task_id() - 1;
 				let completed_hash = H256([123; 32]);
+
+		// Benchmark the submission of a completed task
 		}: _(RawOrigin::Signed(executor.clone()), task_id, completed_hash)
 		verify {
+				// Verify that the task status is updated and the completed hash is recorded.
 				let task_status = TaskManagementModule::<T>::task_status(task_id).unwrap();
 				assert_eq!(task_status, TaskStatusType::PendingValidation);
 
@@ -65,78 +101,90 @@ benchmarks! {
 				assert_eq!(verifications.verifier.clone().unwrap().completed_hash, None);
 		}
 
+		// Benchmark for verifying a completed task
 		verify_completed_task {
 				let s in 0 .. 100;
+				
+				// Retrieve a whitelisted account to use as the caller (sender of the transaction)
+				// This is a pre-authorized account used for benchmarking purposes.
 				let caller: T::AccountId = whitelisted_caller();
 				let executor: T::AccountId = account("executor", 0, 0);
 				let verifier: T::AccountId = account("verifier", 0, 0);
 
-				// Register a worker for the executor
+				// Register the executor worker and schedule the task.
+				let domain=get_domain("https://api-worker.testing");				
 				let api_info_executor = pallet_edge_connect::types::WorkerAPI {
-						ip: None,
-						domain: Some(BoundedVec::try_from(b"https://api-worker.testing".to_vec()).unwrap())
+						domain,
 				};
-				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(executor.clone()).into(), api_info_executor.ip, api_info_executor.domain)?;
+				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(executor.clone()).into(), api_info_executor.domain)?;
 
 				// Create a task data
-				let task_data = BoundedVec::try_from(b"some-docker-imgv.0".to_vec()).unwrap();
+				let task_data = get_task_data("some-docker-imgv.0");
 				TaskManagementModule::<T>::task_scheduler(RawOrigin::Signed(caller.clone()).into(), task_data.clone())?;
 
-				// Register a worker for the verifier
+				// Register the verifier and complete the task by the executor.
+				let domain2=get_domain("https://api-worker2.testing");
+
 				let api_info_verifier = pallet_edge_connect::types::WorkerAPI {
-						ip: None,
-						domain: Some(BoundedVec::try_from(b"https://api-worker2.testing".to_vec()).unwrap())
+						domain:domain2,
 				};
-				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(verifier.clone()).into(), api_info_verifier.ip, api_info_verifier.domain)?;
+				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(verifier.clone()).into(), api_info_verifier.domain)?;
 
 				let task_id = TaskManagementModule::<T>::next_task_id() - 1;
 
 				let completed_hash = H256([123; 32]);
 				TaskManagementModule::<T>::submit_completed_task(RawOrigin::Signed(executor.clone()).into(), task_id, completed_hash)?;
 
+		// Benchmark the verification of a completed task
 		}: _(RawOrigin::Signed(verifier.clone()), task_id, completed_hash)
 		verify {
+				// Verify the task status is updated and marked as completed.
 				let task_status = TaskManagementModule::<T>::task_status(task_id).unwrap();
 				assert_eq!(task_status, TaskStatusType::Completed);
 		}
 
+		// Benchmark for resolving a completed task by the resolver
 		resolve_completed_task {
 				let s in 0 .. 100;
+				
+				// Retrieve a whitelisted account to use as the caller (sender of the transaction)
+				// This is a pre-authorized account used for benchmarking purposes.
 				let caller: T::AccountId = whitelisted_caller();
 
 				let executor: T::AccountId = account("executor", 0, 0);
 				let verifier: T::AccountId = account("verifier", 0, 0);
 				let resolver: T::AccountId = account("resolver", 0, 0);
 
-				// Register a worker for the executor
+				// Register workers for the executor, verifier, and resolver.
+				let domain=get_domain("https://api-worker.testing");
 				let api_info_executor = pallet_edge_connect::types::WorkerAPI {
-						ip: None,
-						domain: Some(BoundedVec::try_from(b"https://api-worker.testing".to_vec()).unwrap())
+						domain,
 				};
-				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(executor.clone()).into(), api_info_executor.ip, api_info_executor.domain)?;
+				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(executor.clone()).into(), api_info_executor.domain)?;
 
-				// Create a task data
-				let task_data = BoundedVec::try_from(b"some-docker-imgv.0".to_vec()).unwrap();
+				// Schedule a task.
+				let task_data = get_task_data("some-docker-imgv.0");
 				TaskManagementModule::<T>::task_scheduler(RawOrigin::Signed(caller.clone()).into(), task_data.clone())?;
 				let task_id = TaskManagementModule::<T>::next_task_id() - 1;
 
-				// Register a worker for the verifier
+				// Register verifier and submit completed task by the executor.
+				let domain2=get_domain("https://api-worker2.testing");
 				let api_info_verifier = pallet_edge_connect::types::WorkerAPI {
-						ip: None,
-						domain: Some(BoundedVec::try_from(b"https://api-worker2.testing".to_vec()).unwrap())
+						domain:domain2,
 				};
-				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(verifier.clone()).into(), api_info_verifier.ip, api_info_verifier.domain)?;
+				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(verifier.clone()).into(), api_info_verifier.domain)?;
 
 				// Submit completed task by the executor
 				let completed_hash = H256([4; 32]);
 				TaskManagementModule::<T>::submit_completed_task(RawOrigin::Signed(executor.clone()).into(), task_id, completed_hash)?;
 
-				// Register a worker for the resolver
+				// Register the resolver.
+				let domain3=get_domain("https://api-worker3.testing");
+
 				let api_info_resolver = pallet_edge_connect::types::WorkerAPI {
-						ip: None,
-						domain: Some(BoundedVec::try_from(b"https://api-worker3.testing".to_vec()).unwrap())
+						domain:domain3,
 				};
-				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(resolver.clone()).into(), api_info_resolver.ip, api_info_resolver.domain)?;
+				pallet_edge_connect::Pallet::<T>::register_worker(RawOrigin::Signed(resolver.clone()).into(), api_info_resolver.domain)?;
 
 				let completed_differing_hash = H256([123; 32]);
 				// Verify completed task by the verifier
@@ -148,8 +196,10 @@ benchmarks! {
 				assert_eq!(verifications.verifier.clone().unwrap().account, verifier);
 				assert_eq!(verifications.verifier.clone().unwrap().completed_hash, Some(completed_differing_hash));
 
+		// Benchmark the resolution of the task by the resolver
 		}: _(RawOrigin::Signed(resolver.clone()), task_id, completed_differing_hash)
 		verify {
+				// Verify the task status is updated to completed.
 				let task_status = TaskManagementModule::<T>::task_status(task_id).unwrap();
 				assert_eq!(task_status, TaskStatusType::Completed);
 		}
