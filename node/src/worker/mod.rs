32c0651b8fa6b438a43f95eda8effcad055bc246
Noor Mohammed <noormohammedb@protonmail.com>
2024-09-12 13:56:53 +0400

    worker register WIP
    decoded WorkerRegistered event
    saved registered worker as json
    ignored json

--- node/src/worker/mod.rs
+++ node/src/worker/mod.rs
@@ -1,13 +1,25 @@
 use std::sync::Arc;
 
+use codec::{Decode, Encode};
 use cyborg_runtime::apis::TaskManagementEventsApi;
 use sc_client_api::BlockchainEvents;
+use serde::{Deserialize, Serialize};
 use sp_api::ProvideRuntimeApi;
 use sp_blockchain::HeaderBackend;
+use sp_core::sr25519;
 use sp_runtime::traits::Block;
 
 pub mod custom_event_listener;
 pub mod register_worker;
+
+// datastructure for worker registartion persistence
+#[derive(Debug, Clone, PartialEq, Eq, Decode, Encode, Serialize, Deserialize)]
+pub struct WorkerData {
+	creator: String,
+	worker: (sr25519::Public, u64),
+	domain: String,
+	domain_encoded: Vec<u8>,
+}
 
 pub async fn start_worker<T, U>(client: Arc<T>)
 where
--- node/src/worker/register_worker.rs
+++ node/src/worker/register_worker.rs
@@ -1,16 +1,34 @@
+use codec::Decode;
 use cyborg_runtime as runtime;
+use sp_core::crypto::Ss58Codec;
+use sp_core::hexdisplay::AsBytesRef;
 use sp_core::sr25519;
+use sp_core::ConstU32;
 use sp_core::Pair;
 use sp_runtime::BoundedVec;
 use std::env;
+use std::io::Write;
+use substrate_api_client::ac_node_api::StaticEvent;
 use substrate_api_client::ac_primitives::DefaultRuntimeConfig;
 use substrate_api_client::SubmitAndWatch;
 use substrate_api_client::XtStatus;
 use substrate_api_client::{rpc::TungsteniteRpcClient, Api};
 
-pub async fn register_worker_on_chain() {
-	// TODO: get all the extrinsic params from environment variables (domain)
+use super::WorkerData;
 
+#[derive(Debug, Clone, PartialEq, Eq, Decode)]
+struct EventWorkerRegistered {
+	creator: sr25519::Public,
+	worker: (sr25519::Public, u64),
+	domain: BoundedVec<u8, ConstU32<128>>,
+}
+
+impl StaticEvent for EventWorkerRegistered {
+	const PALLET: &'static str = "EdgeConnect";
+	const EVENT: &'static str = "WorkerRegistered";
+}
+
+pub async fn register_worker_on_chain() {
 	dbg!("============register_worker_on_chain============");
 
 	// export CYBORG_WORKER_KEY="e5be9a5092b81bca64be81d212e7f2f9eba183bb7a90954f7b76361f6edb5c0a" # //Alice
@@ -32,7 +50,6 @@ pub async fn register_worker_on_chain() {
 
 	let mut api = Api::<DefaultRuntimeConfig, _>::new(client).unwrap();
 
-	dbg!(key.public());
 	api.set_signer(key.clone().into());
 
 	let domain = BoundedVec::try_from(worker_domain.as_bytes().to_vec()).unwrap();
@@ -46,11 +63,28 @@ pub async fn register_worker_on_chain() {
 	dbg!(&tr_tx);
 
 	let ext_response = api.submit_and_watch_extrinsic_until(tr_tx, XtStatus::InBlock);
-	dbg!(&ext_response);
+	println!("{:?}", &ext_response);
 
 	match ext_response {
-		Ok(_) => {
+		Ok(ext_response_succ) => {
+			println!("{:?}", &ext_response_succ);
 			println!("Worker registered successfully ✅✅✅✅✅✅✅✅✅✅✅✅✅");
+			println!("Events: {:?}", ext_response_succ.events);
+
+			for event in ext_response_succ.events.unwrap() {
+				if event.pallet_name() == "EdgeConnect" {
+					let decoded_event = event.as_event::<EventWorkerRegistered>();
+					dbg!(&decoded_event);
+					match decoded_event {
+						Err(_) | Ok(None) => {
+							println!("❌Event not decoded properly❌");
+						}
+						Ok(Some(registration_event)) => {
+							worker_retain_after_restart(registration_event);
+						}
+					}
+				}
+			}
 		}
 		Err(e) => {
 			println!("Somethign went wrong while registering worker ❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌");
@@ -58,4 +92,34 @@ pub async fn register_worker_on_chain() {
 			println!("RESTART The worker node with proper environment variables");
 		}
 	}
+}
+
+fn worker_retain_after_restart(reg_event: EventWorkerRegistered) {
+	let registered_worker_data = WorkerData {
+		creator: reg_event.creator.to_ss58check(),
+		worker: reg_event.worker,
+		domain: String::from_utf8_lossy(reg_event.domain.as_bytes_ref()).to_string(),
+		domain_encoded: reg_event.domain.into(),
+	};
+
+	let registered_worker_json = serde_json::to_string_pretty(&registered_worker_data);
+	dbg!(&registered_worker_json);
+
+	use std::{fs::File, path::Path};
+
+	let config_path = Path::new("registered_worker_config.json");
+	match File::create(config_path) {
+		Err(e) => {
+			dbg!(e);
+		}
+		Ok(mut created_file) => {
+			created_file
+				.write_all(registered_worker_json.unwrap().as_bytes())
+				.unwrap_or_else(|_| panic!("Unable to write file : {:?}", config_path.to_str()));
+			println!(
+				"✅✅Saved worker registration data to file: {:?}✅✅ ",
+				config_path.to_str()
+			);
+		}
+	};
 }
