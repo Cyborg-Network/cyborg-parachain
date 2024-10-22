676b636da1265633fa1245d100d74529afb29e54
Tom Bleek <tom.bleek@proton.me>
2024-10-22 14:53:01 +0200

    Added token symbol

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -65,7 +65,7 @@ pub fn template_session_keys(keys: AuraId) -> runtime::SessionKeys {
 pub fn development_config() -> ChainSpec {
 	// Give your base currency a unit name and decimal places
 	let mut properties = sc_chain_spec::Properties::new();
-	properties.insert("tokenSymbol".into(), "ENTT".into());
+	properties.insert("tokenSymbol".into(), "BORG".into());
 	properties.insert("tokenDecimals".into(), 12.into());
 	properties.insert("ss58Format".into(), 42.into());
 
@@ -109,13 +109,14 @@ pub fn development_config() -> ChainSpec {
 		get_account_id_from_seed::<sr25519::Public>("Alice"),
 		1000.into(),
 	))
+  .with_properties(properties)
 	.build()
 }
 
 pub fn local_testnet_config() -> ChainSpec {
 	// Give your base currency a unit name and decimal places
 	let mut properties = sc_chain_spec::Properties::new();
-	properties.insert("tokenSymbol".into(), "ENTT".into());
+	properties.insert("tokenSymbol".into(), "BORG".into());
 	properties.insert("tokenDecimals".into(), 12.into());
 	properties.insert("ss58Format".into(), 42.into());
 
