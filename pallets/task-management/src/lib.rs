61d57f0be155ec21fe30677cba979cec0e0322ee
Tom Bleek <tom.bleek@proton.me>
2025-11-17 17:29:05 +0100

    Add cycloud task types and task data

--- pallets/task-management/src/lib.rs
+++ pallets/task-management/src/lib.rs
@@ -258,7 +258,7 @@ where
 				TaskSubmissionData::FlashInfer(_) => {
 					MinerType::Edge
 				}
-				TaskSubmissionData::CyCloud => {
+				TaskSubmissionData::CyCloud(_) => {
 					MinerType::Cloud
 				},
 			};
